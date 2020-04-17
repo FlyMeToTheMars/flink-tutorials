f3e564a0f46d8626ef7dd1773996a7fb4d2eca65
Fucun Chu <chufucun@hotmail.com>
2020-04-17 22:32:01 +0800

    init

---
+++ flink-parquet/src/main/java/io/github/streaming/examples/flink/utils/Constants.java
@@ -0,0 +1,15 @@
+package io.github.streaming.examples.flink.utils;
+
+public class Constants {
+
+    public static final String SENSITIVE_KEYS_KEY = "sensitive.keys" ;
+    public static final String SENSITIVE_KEYS_KEY_DEFAULR = "password" ;
+    public static final String MASK = "********" ;
+    public static final String KAFKA_PREFIX = "kafka." ;
+    public static final String K_KAFKA_TOPIC = "kafkaTopic" ;
+    public static final String K_HDFS_OUTPUT = "hdfsOutput" ;
+
+
+    public static final String K_PROPERTIES_FILE = "properties.file" ;
+
+}
