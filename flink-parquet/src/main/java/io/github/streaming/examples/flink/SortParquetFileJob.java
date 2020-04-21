d764ad467b67ef0ac2a42154e089c9e3d1bdb70d
Fucun Chu <chufucun@hotmail.com>
2020-04-21 15:45:50 +0800

    offline sort

--- flink-parquet/src/main/java/io/github/streaming/examples/flink/SortParquetFileJob.java
+++ flink-parquet/src/main/java/io/github/streaming/examples/flink/SortParquetFileJob.java
@@ -3,28 +3,22 @@ package io.github.streaming.examples.flink;
 import com.cloudera.streaming.examples.flink.types.ItemTransaction;
 import io.github.streaming.examples.flink.utils.Utils;
 import java.io.File;
-import java.util.PriorityQueue;
 import org.apache.avro.Schema;
 import org.apache.avro.reflect.ReflectData;
 import org.apache.flink.api.common.io.FileInputFormat;
 import org.apache.flink.api.common.operators.Order;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.typeutils.PojoTypeInfo;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.formats.parquet.ParquetPojoInputFormat;
 import org.apache.flink.formats.parquet.ParquetRowInputFormat;
-import org.apache.flink.streaming.api.TimerService;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
-import org.apache.flink.streaming.api.watermark.Watermark;
-import org.apache.flink.util.Collector;
+import org.apache.flink.table.expressions.E;
 import org.apache.parquet.avro.AvroSchemaConverter;
 import org.apache.parquet.schema.MessageType;
 
@@ -50,76 +44,19 @@ public class SortParquetFileJob extends BaseJob {
 
     MessageType msgType = SCHEMA_CONVERTER.convert(avroSchema);
 
-    FileInputFormat format = new ParquetRowInputFormat(Path.fromLocalFile(partFile), msgType);
+    FileInputFormat inputFormat = new ParquetRowInputFormat(Path.fromLocalFile(partFile), msgType);
+
+    inputFormat = new ParquetPojoInputFormat(Path.fromLocalFile(partFile), msgType,
+        (PojoTypeInfo<ItemTransaction>) PojoTypeInfo.of(ItemTransaction.class));
 
     DataSource source = env
-        .readFile(format, partFile.getPath());
+        .createInput(inputFormat, TypeInformation.of(ItemTransaction.class));
 
     source
         .sortPartition("itemId", Order.ASCENDING)
-        .print();
+        .writeAsCsv("/tmp/flink/item.csv", WriteMode.OVERWRITE);
 
     env.execute("Flink Streaming Sort Parquet Job");
   }
 
-  public static class SortFunction extends
-      KeyedProcessFunction<String, ItemTransaction, ItemTransaction> {
-
-    private ValueState<PriorityQueue<ItemTransaction>> queueState = null;
-
-    @Override
-    public void open(Configuration config) {
-      ValueStateDescriptor<PriorityQueue<ItemTransaction>> descriptor = new ValueStateDescriptor<>(
-          // state name
-          "sorted-events",
-          // type information of state
-          TypeInformation.of(new TypeHint<PriorityQueue<ItemTransaction>>() {
-          }));
-      queueState = getRuntimeContext().getState(descriptor);
-    }
-
-    @Override
-    public void processElement(ItemTransaction event, Context context,
-        Collector<ItemTransaction> out) throws Exception {
-      TimerService timerService = context.timerService();
-
-      if (context.timestamp() > timerService.currentWatermark()) {
-        PriorityQueue<ItemTransaction> queue = queueState.value();
-        if (queue == null) {
-          queue = new PriorityQueue<>(10);
-        }
-        queue.add(event);
-        queueState.update(queue);
-        timerService.registerEventTimeTimer(event.ts);
-      }
-    }
-
-    @Override
-    public void onTimer(long timestamp, OnTimerContext context, Collector<ItemTransaction> out)
-        throws Exception {
-      PriorityQueue<ItemTransaction> queue = queueState.value();
-      Long watermark = context.timerService().currentWatermark();
-      ItemTransaction head = queue.peek();
-      while (head != null && head.ts <= watermark) {
-        out.collect(head);
-        queue.remove(head);
-        head = queue.peek();
-      }
-    }
-  }
-
-  public static class TransactionAssigner implements
-      AssignerWithPunctuatedWatermarks<ItemTransaction> {
-
-    @Override
-    public long extractTimestamp(ItemTransaction event, long previousElementTimestamp) {
-      return event.ts;
-    }
-
-    @Override
-    public Watermark checkAndGetNextWatermark(ItemTransaction event, long extractedTimestamp) {
-      // simply emit a watermark with every event
-      return new Watermark(extractedTimestamp - 30000);
-    }
-  }
 }
