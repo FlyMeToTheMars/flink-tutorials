f13b474b27b6c29e115652e8cc6d0b2bb64e9de8
Fucun Chu <chufucun@hotmail.com>
2020-05-06 16:23:41 +0800

    add test for column-index

--- flink-parquet/src/test/java/io/github/streaming/examples/flink/ParquetFileTest.java
+++ flink-parquet/src/test/java/io/github/streaming/examples/flink/ParquetFileTest.java
@@ -1,15 +1,31 @@
 package io.github.streaming.examples.flink;
 
+import com.cloudera.streaming.examples.flink.types.ItemTransaction;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import io.github.streaming.examples.flink.utils.parquet.Parquet;
 import io.github.streaming.examples.flink.utils.parquet.ParquetReaderUtils;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import org.apache.avro.Schema;
+import org.apache.avro.reflect.ReflectData;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroSchemaConverter;
+import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.tools.Main;
+import org.checkerframework.checker.units.qual.Temperature;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,31 +33,51 @@ import org.junit.Test;
 public class ParquetFileTest {
 
   private Configuration conf = new Configuration();
+  private Path basepath;
   private Path inputPath;
 
   @Before
   public void pre() throws IOException {
-    String basepath = "/tmp/flink/dt=20200419/";
-    File partFile = new File(basepath, "part-0-0");
+    String basedir = "/tmp/flink/dt=20200506/";
+    basepath = new Path(new File(basedir).getPath());
+    File partFile = new File(basedir, "part-0-0");
     inputPath = new Path(partFile.getPath());
 
     if (!inputPath.getFileSystem(conf).exists(inputPath)) {
-      System.out.println("File Not Found!" + inputPath.toUri());
+      Assert.fail("File Not Found!" + inputPath.toUri());
     }
+    System.out.println("basepath: " + basepath);
+    System.out.println("inputPath: " + inputPath);
   }
 
+  @Test
+  public void restRead() {
+  }
 
   @Test
   public void allmeta() throws IOException {
-    Parquet parquet = ParquetReaderUtils.getParquetData(inputPath.toUri().getPath());
 
-    System.out.println("Schema: " + parquet.getSchema());
+    FileStatus[] filelist = basepath.getFileSystem(conf).listStatus(basepath);
+    for (FileStatus fileStatus : filelist) {
+      if (StringUtils.endsWith(fileStatus.getPath().getName(), "-0-0")) {
 
-    List<SimpleGroup> data = parquet.getData();
-    for (SimpleGroup group : data) {
+        Parquet parquet = ParquetReaderUtils.getParquetData(fileStatus.getPath().toUri().getPath());
+        ParquetFileReader.readFooters(conf, fileStatus, false);
+//        System.out.println("Schema: " + parquet.getSchema());
 
+        List<SimpleGroup> data = parquet.getData();
+        Set<String> itemids = Sets.newHashSet();
+        for (SimpleGroup group : data) {
+          String itemId = group.getString(2, 0);
+          itemids.add(itemId);
+          System.out.println(fileStatus.getPath().getName() + ": " + itemId);
+        }
+
+        List<String> itemidList = Lists.newArrayList(itemids);
+        Collections.sort(itemidList);
+        System.out.println(fileStatus.getPath().getName() + " : " + itemidList);
+      }
     }
-
   }
 
   @Test
@@ -67,6 +103,14 @@ public class ParquetFileTest {
   @Test
   public void rowCount() throws IOException {
     String[] args = Lists.newArrayList("rowcount", "-d", inputPath.toUri().getPath())
+        .toArray(new String[]{});
+    Main.main(args);
+  }
+
+  @Test
+  public void columnIndex() throws IOException {
+    String[] args = Lists
+        .newArrayList("column-index", "-c", "itemId", "-i", inputPath.toUri().getPath())
         .toArray(new String[]{});
     Main.main(args);
   }
